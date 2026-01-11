0a18013e4679f6b93cff36391f60bac59465edb1
Danylo Nikulin <nikulin.danilo@gmail.com>
2026-01-11 17:07:22 +0100

    Navbar fix3 in Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,21 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer"; // <-- импортируем футер
+import Footer from "../components/Footer"; // подключаем футер
 
 export default function HomePage() {
-  const scrollTo = (id: string) => {
-    const el = document.getElementById(id);
-    if (el) el.scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
     <main className="relative">
       {/* Navbar */}
-      <Navbar onScroll={scrollTo} />
+      <Navbar />
+
       {/* Hero секция */}
       <section id="hero" className="min-h-screen relative">
-        <Hero onScroll={scrollTo} />
+        <Hero />
       </section>
+
       {/* Footer с контактами */}
-      <Footer /> {/* <-- подключаем футер */}
+      <Footer />
     </main>
   );
 }
